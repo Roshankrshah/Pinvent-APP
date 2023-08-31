c4940e908ceccac158a5587d221ed735906baa91
Roshan <shahroshan0720@gmail.com>
2023-08-31 17:30:14 +0530

    login route setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,13 @@
 const { StatusCodes } = require('http-status-codes');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SEC, { expiresIn: '1d' })
 }
 
-
+// Register User
 const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -48,6 +49,49 @@ const registerUser = async (req, res) => {
 
 };
 
+// Login User
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(StatusCodes.BAD_REQUEST);
+        throw new Error("Please provide email and password");
+    }
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(StatusCodes.BAD_REQUEST);
+        throw new Error("User not Found, please signup");
+    }
+
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    if (user && passwordIsCorrect) {
+        const token = generateToken(user._id);
+        res.cookie("token", token, {
+            path: "/",
+            httpOnly: true,
+            expires: new Date(Date.now() + 1000 * 86400),
+            /*sameSite: "none",
+            secure: true*/
+        })
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(StatusCodes.OK).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+            token
+        });
+    } else {
+        res.status(StatusCodes.UNAUTHORIZED);
+        throw new Error("Invalid email or password");
+    }
+}
+
 module.exports = {
     registerUser,
+    loginUser
 };
