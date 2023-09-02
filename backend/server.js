5a2fa290f92c30d5dc703d95bfe7abb286685342
Roshan <shahroshan0720@gmail.com>
2023-09-02 13:36:21 +0530

    cloudinary setup

--- backend/server.js
+++ backend/server.js
@@ -6,37 +6,48 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoute = require('./routes/userRoutes');
 const contactRoute = require('./routes/contactRoute');
+const productRoute = require('./routes/productRoute');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const cookieParser = require('cookie-parser');
 
+const cloudinary = require('cloudinary').v2;
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
 const app = express();
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Personal inventory starting');
 });
 
-app.use('/api/users',userRoute);
-app.use('/api/contactus',contactRoute);
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/contactus', contactRoute);
 
 app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3202;
 
-const start = ()=>{
+const start = () => {
     mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        app.listen(PORT,()=>{
-            console.log(`Server listening at http://localhost:${PORT}`);
+        .then(() => {
+            app.listen(PORT, () => {
+                console.log(`Server listening at http://localhost:${PORT}`);
+            })
         })
-    })
-    .catch(err=>{
-        console.log(err);
-    })
+        .catch(err => {
+            console.log(err);
+        })
 };
 
 start();
