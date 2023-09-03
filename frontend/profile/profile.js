63134325fe59b3545cda83d5cb78e18c1c961d5c
Roshan <shahroshan0720@gmail.com>
2023-09-03 15:06:44 +0530

    edit profile setup

---
+++ frontend/profile/profile.js
@@ -0,0 +1,80 @@
+const userDetails = document.querySelector('.user-details');
+const profileImage = document.querySelector('.profile-image');
+const updateDetails = document.querySelector('.update-user-details');
+const updateImage = document.querySelector('.update-profile-image');
+
+const start = async () => {
+    const res = await fetch('http://localhost:3202/api/users/getuser', {
+        credentials: 'include'
+    });
+    const resData = await res.json();
+    console.log(resData);
+    userDetails.innerHTML = `
+        <p><strong>Name:</strong> ${resData.name}</p>
+        <p><strong>Email:</strong> ${resData.email}</p>
+        <p><strong>Phone:</strong> ${resData.phone ? resData.phone : ''}</p>
+        <p><strong>Bio:</strong> ${resData.bio}</p>
+        `;
+    profileImage.setAttribute('src', `${resData.photo}`);
+
+    updateImage.setAttribute('src', `${resData.photo}`);
+    updateDetails.innerHTML = `
+    <form>
+        <div class="mb-3">
+            <label for="InputName" class="form-label">Name</label>
+            <input type="text" class="form-control" id="InputName" value="${resData.name}">
+        </div>
+        <div class="mb-3">
+            <label for="InputEmail" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="InputEmail" value="${resData.email}" disabled>
+        </div>
+        <div class="mb-3">
+            <label for="InputPhone" class="form-label">Phone</label>
+            <input type="text" class="form-control" id="InputPhone" value="${resData.phone ? resData.phone : ''}">
+        </div>
+        <div class="mb-3">
+            <label for="bioTextarea" class="form-label">Bio</label>
+            <textarea class="form-control" id="bioTextarea" rows="3">${resData.bio}</textarea>
+        </div>
+        <div class="mb-3">
+            <label for="formFile" class="form-label">Photo</label>
+            <input class="form-control" type="file" id="formFile">
+        </div>
+        <button type="submit" class="btn btn-primary updatebtn">Save</button>
+    </form>`;
+
+    const newImage = document.getElementById('formFile');
+
+    const updateBtn = document.querySelector('.updatebtn');
+
+    updateBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const name = document.getElementById('InputName').value;
+        const email = document.getElementById('InputEmail').value;
+        const phone = document.getElementById('InputPhone').value;
+        const bio = document.getElementById('bioTextarea').value;
+        //const photo = document.getElementById('InputName').value;
+        console.log(name, email, phone, bio);
+        const updateUser = {name:name,email:email,phone: phone,bio:bio};
+
+        const res = await fetch('http://localhost:3202/api/users/updateuser', {
+            method: 'PATCH',
+            body: JSON.stringify(updateUser),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+        });
+
+        const resData = await res.json();
+        console.log(resData);
+        //alert('updated');
+        location.reload();
+    })
+}
+
+start();
+
+
+
+
