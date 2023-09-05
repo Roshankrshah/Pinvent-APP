876083e26be7ec302635ca721fce10f80e5dcca9
Roshan <shahroshan0720@gmail.com>
2023-09-05 11:04:15 +0530

    forgot password setup

---
+++ frontend/password/password.js
@@ -0,0 +1,22 @@
+const resetBtn = document.querySelector('.reset-btn');
+const backBtn = document.querySelector('.back-btn');
+
+backBtn.addEventListener('click',()=>{
+    window.location.href = 'http://127.0.0.1:5500/frontend/home.html';
+});
+
+resetBtn.addEventListener('click',async (e)=>{
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const user = {email:email};
+    const resp = await fetch('http://localhost:3202/api/users/forgotpassword',{
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include'
+    });
+    const resData = await resp.json();
+    alert(resData.message);
+})
