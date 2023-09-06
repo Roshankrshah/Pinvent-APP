3b5484deb20e21ca8b1c7c8e2c0693cc83917b7a
Roshan <shahroshan0720@gmail.com>
2023-09-06 11:30:18 +0530

    inventory stats setup

--- frontend/dashboard/dashboard.js
+++ frontend/dashboard/dashboard.js
@@ -12,3 +12,36 @@ logoutBtn.addEventListener('click',()=>{
     console.log(document.cookie);
     alert('logout')
 });
+
+const start = async() => {
+    const res = await fetch('http://localhost:3202/api/products',{
+        credentials: 'include'
+    });
+    const resData = await res.json();
+    console.log(resData);
+    const totalProduct = resData.length;
+    let category = new Set();
+    let totalCost = 0;
+    let outOfStock = 0;
+
+    resData.forEach((product)=>{
+        category.add(product.category);
+        totalCost += (Number(product.quantity) * Number(product.price));
+        if(product.quantity == 0){
+            outOfStock += 1;
+        }
+    });
+
+    console.log(totalProduct, category,totalCost,outOfStock);
+    const firstBox = document.querySelector('.first-box');
+    const secondBox = document.querySelector('.second-box');
+    const thirdBox = document.querySelector('.third-box');
+    const fourthBox = document.querySelector('.fourth-box');
+
+    firstBox.innerHTML = `Total Products <br> ${totalProduct}`;
+    secondBox.innerHTML = `Total Store Value <br> ${category.size}`;
+    thirdBox.innerHTML = `Out of Stock <br> ${totalCost}`;
+    fourthBox.innerHTML = `All Categories <br> ${outOfStock}`;
+};
+
+start();
