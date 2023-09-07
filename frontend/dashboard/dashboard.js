161217ca5123dd45a4154315b7f57ee28bcdf84f
Roshan <shahroshan0720@gmail.com>
2023-09-07 11:45:23 +0530

    view product setup

--- frontend/dashboard/dashboard.js
+++ frontend/dashboard/dashboard.js
@@ -56,14 +56,109 @@ const start = async () => {
                 <td>${product.quantity}</td>
                 <td>${product.quantity * product.price}</td>
                 <td>
-                    <span class="view"><i class="fa-regular fa-eye"></i></span> 
-                    <span class="edit"><i class="fa-regular fa-pen-to-square"></i></span> 
-                    <span class="delete"><i class="fa-regular fa-trash-can"></i></span>
+                    <button class="btn view" data-id="${product._id}"><i class="fa-regular fa-eye"></i> </button>
+                    <button class="edit" data-id="${product._id}"><i class="fa-regular fa-pen-to-square"></i></button> 
+                    <button class="delete" data-id="${product._id}"><i class="fa-regular fa-trash-can"></i></button>
                 </td>
             </tr>`;
 
         productTable.innerHTML += tableRow;
-    })
+    });
+
+    const viewBtns = document.querySelectorAll('.view');
+    const editBtns = document.querySelectorAll('.edit');
+    const deleteBtns = document.querySelectorAll('.delete');
+
+    viewBtns.forEach(viewBtn => {
+        viewBtn.addEventListener('click', viewProduct);
+    });
+    editBtns.forEach(editBtn => {
+        editBtn.addEventListener('click', editProduct);
+    });
+    deleteBtns.forEach(deleteBtn=>{
+        deleteBtn.addEventListener('click', deleteProduct);
+    });
 };
 
+const modalAddition = (resData) => {
+    const openModal = document.querySelector('.openModal');
+    const viewModal = document.createElement('div');
+    viewModal.classList.add('modal-overlay');
+    viewModal.classList.add('open-modal');
+    viewModal.innerHTML = `
+    <div class="modal-container">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Product Details</h5>
+            </div>
+            <div class="modal-body">
+                       
+                <p>
+                    <strong>Product Image</strong><br> <img src="${resData.image.filePath}" width=200 height =200><br>
+                    <strong>Product Name:</strong> ${resData.name}<br>
+                    <strong>Product Category:</strong> ${resData.category}<br>
+                    <strong>Product Price:</strong> ₹${resData.price}<br>
+                    <strong>Product Quantity:</strong> ${resData.quantity}<br>
+                    <strong>Product Description:</strong> ${resData.description}<br>
+                    <strong>Product Added On:</strong> ${new Date(resData.createdAt).toLocaleDateString()}<br>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary viewclose-btn" data-bs-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+    </div>`;
+    openModal.appendChild(viewModal);
+    let viewcloseBtn = document.querySelector(".viewclose-btn");
+
+    viewcloseBtn.addEventListener("click", () => {
+        openModal.removeChild(viewModal);
+    });
+}
+
+const viewProduct = async (e) => {
+    const res = await fetch(`http://localhost:3202/api/products/${e.currentTarget.dataset.id}`, {
+        credentials: 'include'
+    });
+    const resData = await res.json();
+    console.log(resData);
+    modalAddition(resData);
+}
+
+const editProduct = (e) => {
+    console.log(e.currentTarget.dataset.id);
+    const openModal = document.querySelector('.openModal');
+    const viewModal = document.createElement('div');
+    viewModal.classList.add('modal-overlay');
+    viewModal.classList.add('open-modal');
+    viewModal.innerHTML = `
+    <div class="modal-container">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"></h5>
+            </div>
+            <div class="modal-body">
+                <h2></h2>
+                <h5></h5>
+                <p></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary viewclose-btn" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary book-btn">Book</button>
+            </div>
+        </div>
+    </div>`;
+    openModal.appendChild(viewModal);
+    let viewcloseBtn = document.querySelector(".viewclose-btn");
+
+    viewcloseBtn.addEventListener("click", () => {
+        openModal.removeChild(viewModal);
+    });
+}
+
+const deleteProduct = () => {
+
+}
+
 start();
+
